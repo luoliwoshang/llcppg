2eb4a4a779abf9357fc68c424c4c1741dbacf9c4
luoliwoshang <2643523683@qq.com>
2024-12-24 10:38:18 +0800

    ci:llcppgtest llcppcfg install

--- .github/workflows/install_llcppg_modules.sh
+++ .github/workflows/install_llcppg_modules.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# for test
+go install ./cmd/llcppcfg
+go install ./cmd/llcppgtest
+
+# main process required
 llgo install ./_xtool/llcppsymg
 llgo install ./_xtool/llcppsigfetch
 go install ./cmd/gogensig
