c97c0fc555d427915915f761aaf4e4daaf85844c
luoliwoshang <2643523683@qq.com>
2025-01-24 16:44:46 +0800

    llcppg/codegen:avoid re typedef in gogensig for correct USR ref

---
+++ cmd/gogensig/convert/_testdata/typedef/hfile/temp.h
@@ -0,0 +1,27 @@
+typedef struct {
+    long a;
+} a;
+
+typedef struct b {
+    long a;
+} b;
+
+typedef union {
+    long a;
+    long b;
+} c;
+
+typedef union d {
+    long a;
+    long b;
+} d;
+
+typedef enum {
+    ITEM_A,
+    ITEM_B,
+} e;
+
+typedef enum f {
+    ITEM_C,
+    ITEM_D,
+} f;
