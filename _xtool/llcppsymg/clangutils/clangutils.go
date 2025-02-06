90ffac47d5ca795644c82e4505615f8119e88e05
luoliwoshang <2643523683@qq.com>
2025-02-06 17:52:50 +0800

    clangutils:ComposeIncludes
    clangutils:Preprocess

--- _xtool/llcppsymg/clangutils/clangutils.go
+++ _xtool/llcppsymg/clangutils/clangutils.go
@@ -2,6 +2,8 @@ package clangutils
 
 import (
 	"errors"
+	"os"
+	"os/exec"
 	"unsafe"
 
 	"github.com/goplus/llgo/c"
@@ -25,11 +27,7 @@ const TEMP_FILE = "temp.h"
 func CreateTranslationUnit(config *Config) (*clang.Index, *clang.TranslationUnit, error) {
 	// default use the c/c++ standard of clang; c:gnu17 c++:gnu++17
 	// https://clang.llvm.org/docs/CommandGuide/clang.html
-	defaultArgs := []string{"-x", "c"}
-	if config.IsCpp {
-		defaultArgs = []string{"-x", "c++"}
-	}
-	allArgs := append(defaultArgs, config.Args...)
+	allArgs := append(defaultArgs(config.IsCpp), config.Args...)
 
 	cArgs := make([]*c.Char, len(allArgs))
 	for i, arg := range allArgs {
@@ -108,4 +106,42 @@ func GetInclusions(unit *clang.TranslationUnit, visitor InclusionVisitor) {
 		cfn := *(*InclusionVisitor)(data)
 		cfn(inced, ics)
 	}, unsafe.Pointer(&visitor))
+}
+
+func defaultArgs(isCpp bool) []string {
+	args := []string{"-x", "c"}
+	if isCpp {
+		args = []string{"-x", "c++"}
+	}
+	return args
+}
+
+// ComposeIncludes create Include list
+// #include <file1.h>
+// #include <file2.h>
+func ComposeIncludes(files []string, outfile string) error {
+	var str string
+	for _, file := range files {
+		str += ("#include <" + file + ">\n")
+	}
+	return os.WriteFile(outfile, []byte(str), 0644)
+}
+
+type PreprocessConfig struct {
+	File    string
+	IsCpp   bool
+	Args    []string
+	OutFile string
+}
+
+func Preprocess(cfg *PreprocessConfig) error {
+	args := []string{"-E"}
+	args = append(args, defaultArgs(cfg.IsCpp)...)
+	args = append(args, cfg.Args...)
+	args = append(args, cfg.File)
+	args = append(args, "-o", cfg.OutFile)
+	cmd := exec.Command("clang", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
