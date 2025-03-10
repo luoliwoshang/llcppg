848cd15ab8a15705b8d657dc4802889b386e02b9
luoliwoshang <2643523683@qq.com>
2025-03-10 21:21:33 +0800

    ci:before release test binaries

---
+++ .github/workflows/test_macos.sh
@@ -0,0 +1,11 @@
+# install demo's lib
+brew install lua zlib isl libgpg-error raylib z3 sqlite3 gmp libxml2 libxslt
+
+export PKG_CONFIG_PATH="/opt/homebrew/opt/zlib/lib/pkgconfig"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/sqlite/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/libxml2/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/libxslt/lib/pkgconfig:$PKG_CONFIG_PATH"
+pkg-config --cflags --libs sqlite3
+pkg-config --cflags --libs libxslt
+
+llcppgtest -demos ./_llcppgtest
