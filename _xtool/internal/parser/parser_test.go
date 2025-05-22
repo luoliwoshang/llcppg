91b4538a0df6607f643ff63503b523b149c63509
luoliwoshang <2643523683@qq.com>
2025-05-22 11:44:56 +0800

    chore:check fail expect

--- _xtool/internal/parser/parser_test.go
+++ _xtool/internal/parser/parser_test.go
@@ -397,7 +397,7 @@ func TestBuiltinType(t *testing.T) {
 		typ      clang.Type
 		expected ast.BuiltinType
 	}{
-		{"Void", btType(clang.TypeVoid), ast.BuiltinType{Kind: ast.Void}},
+		{"Void", btType(clang.TypeVoid), ast.BuiltinType{Kind: ast.Bool}},
 		{"Bool", btType(clang.TypeBool), ast.BuiltinType{Kind: ast.Bool}},
 		{"Char_S", btType(clang.TypeCharS), ast.BuiltinType{Kind: ast.Char, Flags: ast.Signed}},
 		{"Char_U", btType(clang.TypeCharU), ast.BuiltinType{Kind: ast.Char, Flags: ast.Unsigned}},
