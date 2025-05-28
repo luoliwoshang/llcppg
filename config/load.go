2e43918258ebb16f4cfd410ff01073a45a2b5ee8
luoliwoshang <2643523683@qq.com>
2025-05-28 09:46:51 +0800

    chore:loadfile to same file

--- config/load.go
+++ config/load.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strings"
 )
 
+// llcppg.cfg
 func GetConfFromStdin() (conf Config, err error) {
 	return ConfigFromReader(os.Stdin)
 }
@@ -31,6 +34,7 @@ func ConfigFromReader(reader io.Reader) (Config, error) {
 	return config, nil
 }
 
+// llcppg.symb.json
 func GetSymTableFromFile(symFile string) (symTable *SymTable, err error) {
 	bytes, err := os.ReadFile(symFile)
 	if err != nil {
@@ -64,4 +68,57 @@ func (t *SymTable) LookupSymbol(mangle string) (*SymbolInfo, error) {
 		return &symbol, nil
 	}
 	return nil, fmt.Errorf("symbol %s not found", mangle)
+}
+
+// llcppg.pub
+func ReadPubFile(pubfile string) (ret map[string]string, err error) {
+	b, err := os.ReadFile(pubfile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return make(map[string]string), nil
+		}
+		return
+	}
+
+	text := string(b)
+	lines := strings.Split(text, "\n")
+	ret = make(map[string]string, len(lines))
+	for i, line := range lines {
+		flds := strings.Fields(line)
+		goName := ""
+		switch len(flds) {
+		case 1:
+		case 2:
+			goName = flds[1]
+		case 0:
+			continue
+		default:
+			err = fmt.Errorf("%s:%d: too many fields", pubfile, i+1)
+			return
+		}
+		ret[flds[0]] = goName
+	}
+	return
+}
+
+func WritePubFile(file string, public map[string]string) (err error) {
+	if len(public) == 0 {
+		return
+	}
+	f, err := os.Create(file)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	ret := make([]string, 0, len(public))
+	for name, goName := range public {
+		if goName == "" {
+			ret = append(ret, name)
+		} else {
+			ret = append(ret, name+" "+goName)
+		}
+	}
+	sort.Strings(ret)
+	_, err = f.Write([]byte(strings.Join(ret, "\n")))
+	return
 }
