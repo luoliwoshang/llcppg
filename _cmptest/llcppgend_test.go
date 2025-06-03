41a5b19f6a626e6f5f0ccfe84737e7055fc25b6b
luoliwoshang <2643523683@qq.com>
2025-06-03 14:26:13 +0800

    ci:upload to artifacts

--- _cmptest/llcppgend_test.go
+++ _cmptest/llcppgend_test.go
@@ -72,6 +72,7 @@ func TestEnd2End(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(fmt.Sprintf("%s/%s", tc.pkg.Name, tc.pkg.Version), func(t *testing.T) {
+			t.Parallel()
 			testFrom(t, tc, false)
 		})
 	}
