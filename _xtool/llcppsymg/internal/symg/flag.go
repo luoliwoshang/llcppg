f1f42d30d7a6743614dc6b827c0dcf98081f17b2
luoliwoshang <2643523683@qq.com>
2025-06-06 15:27:07 +0800

    refactor(llcppsymg):remove unuse headerfile find

--- _xtool/llcppsymg/internal/symg/flag.go
+++ _xtool/llcppsymg/internal/symg/flag.go
@@ -2,24 +2,14 @@ package symg
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/goplus/llcppg/_xtool/internal/symbol"
 )
 
-// Note: This package is not placed under the 'config' package because 'config'
-// depends on 'cjson'. The parsing of Libs and cflags is intended to be usable
-// by both llgo and go, without introducing additional dependencies.
-
 type Libs struct {
 	Paths []string // Dylib Path
 	Names []string
-}
-
-type CFlags struct {
-	Paths []string // Include Path
 }
 
 func ParseLibs(libs string) *Libs {
@@ -70,44 +60,5 @@ func (l *Libs) Files(findPaths []string, mode LibMode) ([]string, []string, erro
 	if len(foundPaths) == 0 {
 		return nil, notFound, fmt.Errorf("failed to find any libraries")
 	}
-	return foundPaths, notFound, nil
-}
-
-func ParseCFlags(cflags string) *CFlags {
-	parts := strings.Fields(cflags)
-	cf := &CFlags{}
-	for _, part := range parts {
-		if strings.HasPrefix(part, "-I") {
-			cf.Paths = append(cf.Paths, part[2:])
-		}
-	}
-	return cf
-}
-
-func (cf *CFlags) GenHeaderFilePaths(files []string, defaultPaths []string) ([]string, []string, error) {
-	var foundPaths []string
-	var notFound []string
-
-	searchPaths := append(cf.Paths, defaultPaths...)
-
-	for _, file := range files {
-		var found bool
-		for _, path := range searchPaths {
-			fullPath := filepath.Join(path, file)
-			if _, err := os.Stat(fullPath); err == nil {
-				foundPaths = append(foundPaths, fullPath)
-				found = true
-				break
-			}
-		}
-		if !found {
-			notFound = append(notFound, file)
-		}
-	}
-
-	if len(foundPaths) == 0 {
-		return nil, notFound, fmt.Errorf("failed to find any header files")
-	}
-
 	return foundPaths, notFound, nil
 }
